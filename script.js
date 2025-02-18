1d47116e8f1cec647db6f52cc14830c62532c5c9
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2025-02-18 17:36:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-// Create the student object
-const student = {
-  name: "John",
-};
-
 // Function to get all keys from the object
 function getKeys(obj) {
   return Object.keys(obj);
 }
 
-// Export the function
 module.exports = getKeys;
