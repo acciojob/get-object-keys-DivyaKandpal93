12ed208a460699421775a9b277498eda95d66ea9
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2025-02-18 17:35:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Create the student object
- module.exports = getKeys;
 const student = {
   name: "John",
 };
@@ -9,6 +8,5 @@ function getKeys(obj) {
   return Object.keys(obj);
 }
 
-// Testing the function
-let result = getKeys(student); 
-return result;
+// Export the function
+module.exports = getKeys;
