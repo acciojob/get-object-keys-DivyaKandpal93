688299d58341219258adfc21b4d24588b36ee529
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2025-02-18 17:46:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// Function to get all keys from the object
 let student = {
-	name: "Divya"
+  name: "John",
+  age: 30,
+  city: "New York"
 };
 function getKeys(student) {
-  return Object.keys(student);
+return Object.keys(student);
 }
+console.log(getKeys(student)); // Output: ["name", "age", "city"]
